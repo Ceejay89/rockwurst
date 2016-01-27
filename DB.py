20f980803cc32f1c8f75422e3e6e5a4e3bd10955
Ramon Schubert <ramonschubert@gmx.de>
2016-01-27 05:37:05 +0100

    Stuff und so

---
+++ DB.py
@@ -0,0 +1,10 @@
+import sqlite3
+c = sqlite3.connect('example.db')
+c.execute('''CREATE TABLE stocks
+             (date text, trans text, symbol text, qty real, price real)''')
+
+# Insert a row of data
+c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+# Save (commit) the changes
+c.commit()
